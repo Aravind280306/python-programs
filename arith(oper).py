91deaa7dc7da0c700f3239b617a9b3388ccd4d0d
Aravind280306 <aravindv2803@gmail.com>
2025-03-13 14:40:11 +0530

    Add files via upload

---
+++ arith(oper).py
@@ -0,0 +1,12 @@
+#+,-,*,/,//,%,**
+a=int(input("Number 1 : "))
+b=int(input("Number 2 : "))
+print("Arithmetic operators : ")
+print("Addition (+): ",a+b)
+print("Subraction (-) :  ",a-b)
+print("Division( / )",a/b)
+print("Multiplication (*) : ",a*b)
+print("Floor Division  (//)  :  ",a//b)
+print("Modulus (%) : ",a%b)
+print("Exponent (**) :",a**b)
+
