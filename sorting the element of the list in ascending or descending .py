613b4f1d984b37c7024dbb4490a1e2ab99a43789
Aravind280306 <aravindv2803@gmail.com>
2025-03-15 07:05:26 +0530

    Create sorting the element of the list in ascending or descending .py

---
+++ sorting the element of the list in ascending or descending .py
@@ -0,0 +1,7 @@
+#sorting the element of the list in ascending or descending
+lst=[10,9,5,4,3,2,7,8,1,6]
+print('In Ascending order')
+print(sorted(lst))
+print('In Descending order')
+lst.sort(reverse=True)
+print(lst)
